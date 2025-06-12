ab0908db35b85e7e04dded49b9785973a20c8465
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-06-12 18:39:54 +0300

    feat: implement ProductVariant service with CRUD operations

---
+++ Services/Catalog/KMNShop.Catalog/Services/ProductVariantServices/IProductVariantService.cs
@@ -0,0 +1,17 @@
+﻿using KMNShop.Catalog.Dtos.ProductVariantDtos;
+
+namespace KMNShop.Catalog.Services.ProductVariantVariantServices
+{
+    public interface IProductVariantService
+    {
+        Task<List<ResultProductVariantDto>> GetAllProductVariantAsync();
+
+        Task CreateProductVariantAsync(CreateProductVariantDto createProductVariantDto);
+
+        Task UpdateProductVariantAsync(UpdateProductVariantDto updateProductVariantDto);
+
+        Task DeleteProductVariantAsync(string id);
+
+        Task<GetByIdProductVariantDto> GetProductVariantByIdAsync(string id);
+    }
+}
---
+++ Services/Catalog/KMNShop.Catalog/Services/ProductVariantServices/ProductVariantService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using KMNShop.Catalog.Dtos.ProductVariantDtos;
+using KMNShop.Catalog.Entities;
+using KMNShop.Catalog.Settings;
+using MongoDB.Driver;
+
+namespace KMNShop.Catalog.Services.ProductVariantVariantServices
+{
+    public class ProductVariantService : IProductVariantService
+    {
+        private readonly IMongoCollection<ProductVariant> _ProductVariantCollection;
+        private readonly IMapper _mapper;
+
+        public ProductVariantService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _ProductVariantCollection = database.GetCollection<ProductVariant>(databaseSettings.ProductVariantCollectionName);
+
+            _mapper = mapper;
+        }
+
+        public async Task CreateProductVariantAsync(CreateProductVariantDto createProductVariantDto)
+        {
+            var value = _mapper.Map<ProductVariant>(createProductVariantDto);
+
+            await _ProductVariantCollection.InsertOneAsync(value);
+        }
+
+        public async Task DeleteProductVariantAsync(string id)
+        {
+            await _ProductVariantCollection.DeleteOneAsync(x => x.ProductVariantID == id);
+        }
+
+        public async Task<List<ResultProductVariantDto>> GetAllProductVariantAsync()
+        {
+            var values = await _ProductVariantCollection.Find(x => true).ToListAsync();
+
+            return _mapper.Map<List<ResultProductVariantDto>>(values);
+        }
+
+        public async Task<GetByIdProductVariantDto> GetProductVariantByIdAsync(string id)
+        {
+            var value = await _ProductVariantCollection.Find(x => x.ProductVariantID == id).FirstOrDefaultAsync();
+
+            return _mapper.Map<GetByIdProductVariantDto>(value);
+        }
+
+        public async Task UpdateProductVariantAsync(UpdateProductVariantDto updateProductVariantDto)
+        {
+            var value = _mapper.Map<ProductVariant>(updateProductVariantDto);
+
+            await _ProductVariantCollection.FindOneAndReplaceAsync(x => x.ProductVariantID == updateProductVariantDto.ProductVariantID, value);
+        }
+    }
+}
