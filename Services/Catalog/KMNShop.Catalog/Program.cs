34ceb5f4eb8daafaf18b0dcc8898d7aaf7d0890f
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-06-12 18:53:53 +0300

    feat: add service registrations and database configuration to Program.cs

--- Services/Catalog/KMNShop.Catalog/Program.cs
+++ Services/Catalog/KMNShop.Catalog/Program.cs
@@ -1,3 +1,20 @@
+using KMNShop.Catalog.Services.BannerServices;
+using KMNShop.Catalog.Services.BrandServices;
+using KMNShop.Catalog.Services.CategoryServices;
+using KMNShop.Catalog.Services.FooterAboutServices;
+using KMNShop.Catalog.Services.FooterContactServices;
+using KMNShop.Catalog.Services.FooterLinkServices;
+using KMNShop.Catalog.Services.FooterTweetConfigServices;
+using KMNShop.Catalog.Services.PaymentMethodServices;
+using KMNShop.Catalog.Services.ProductDetailServices;
+using KMNShop.Catalog.Services.ProductImageServices;
+using KMNShop.Catalog.Services.ProductServices;
+using KMNShop.Catalog.Services.ProductSpecificationServices;
+using KMNShop.Catalog.Services.ProductVariantVariantServices;
+using KMNShop.Catalog.Services.PromoBannerServices;
+using KMNShop.Catalog.Services.ServiceInfoServices;
+using KMNShop.Catalog.Services.SocialMediaLinkServices;
+using KMNShop.Catalog.Services.SubscriberServices;
 using KMNShop.Catalog.Settings;
 using Microsoft.Extensions.Options;
 using System.Reflection;
@@ -5,6 +22,24 @@ using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IBannerService, BannerService>();
+builder.Services.AddScoped<IBrandService, BrandService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IFooterAboutService, FooterAboutService>();
+builder.Services.AddScoped<IFooterContactService, FooterContactService>();
+builder.Services.AddScoped<IFooterLinkService, FooterLinkService>();
+builder.Services.AddScoped<IFooterTweetConfigService, FooterTweetConfigService>();
+builder.Services.AddScoped<IPaymentMethodService, PaymentMethodService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+builder.Services.AddScoped<IProductImageService, ProductImageService>();
+builder.Services.AddScoped<IProductSpecificationService, ProductSpecificationService>();
+builder.Services.AddScoped<IProductVariantService, ProductVariantService>();
+builder.Services.AddScoped<IPromoBannerService, PromoBannerService>();
+builder.Services.AddScoped<IServiceInfoService, ServiceInfoService>();
+builder.Services.AddScoped<ISocialMediaLinkService, SocialMediaLinkService>();
+builder.Services.AddScoped<ISubscriberService, SubscriberService>();
+
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
