15e84b2d04755d7da2d71403d200bbcf1e5b7dcb
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-06-12 17:53:55 +0300

    feat: implement Category service with CRUD operations

---
+++ Services/Catalog/KMNShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using KMNShop.Catalog.Dtos.CategoryDtos;
+using KMNShop.Catalog.Entities;
+using KMNShop.Catalog.Settings;
+using MongoDB.Driver;
+
+namespace KMNShop.Catalog.Services.CategoryServices
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categoriesCollection;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _categoriesCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+
+            _mapper = mapper;
+        }
+
+        public async Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            var value = _mapper.Map<Category>(createCategoryDto);
+
+            await _categoriesCollection.InsertOneAsync(value);
+        }
+
+        public async Task DeleteCategoryAsync(string id)
+        {
+            await _categoriesCollection.DeleteOneAsync(c => c.CategoryID == id);
+        }
+
+        public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            var values = await _categoriesCollection.Find(x => true).ToListAsync();
+
+            return _mapper.Map<List<ResultCategoryDto>>(values);
+        }
+
+        public async Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id)
+        {
+            var value = await _categoriesCollection.Find(c => c.CategoryID == id).FirstOrDefaultAsync();
+
+            return _mapper.Map<GetByIdCategoryDto>(value);
+        }
+
+        public async Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            var value = _mapper.Map<Category>(updateCategoryDto);
+
+            await _categoriesCollection.FindOneAndReplaceAsync(x => x.CategoryID == updateCategoryDto.CategoryID, value);
+        }
+    }
+}
---
+++ Services/Catalog/KMNShop.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using KMNShop.Catalog.Dtos.CategoryDtos;
+
+namespace KMNShop.Catalog.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+
+        Task DeleteCategoryAsync(string id);
+
+        Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id);
+    }
+}
