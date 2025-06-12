dfe9a4932da66b01e22620331ac95f1ed4ce6805
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-06-12 18:14:21 +0300

    feat: implement FooterTweetConfig service with CRUD operations

---
+++ Services/Catalog/KMNShop.Catalog/Services/FooterTweetConfigServices/FooterTweetConfigService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using KMNShop.Catalog.Dtos.FooterTweetConfigDtos;
+using KMNShop.Catalog.Entities;
+using KMNShop.Catalog.Settings;
+using MongoDB.Driver;
+
+namespace KMNShop.Catalog.Services.FooterTweetConfigServices
+{
+    public class FooterTweetConfigService : IFooterTweetConfigService
+    {
+        private readonly IMongoCollection<FooterTweetConfig> _footerTweetConfigCollection;
+        private readonly IMapper _mapper;
+
+        public FooterTweetConfigService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _footerTweetConfigCollection = database.GetCollection<FooterTweetConfig>(databaseSettings.FooterTweetConfigCollectionName);
+            
+            _mapper = mapper;
+        }
+
+        public async Task CreateFooterTweetConfigAsync(CreateFooterTweetConfigDto createFooterTweetConfigDto)
+        {
+            var value = _mapper.Map<FooterTweetConfig>(createFooterTweetConfigDto);
+
+            await _footerTweetConfigCollection.InsertOneAsync(value);
+        }
+
+        public async Task DeleteFooterTweetConfigAsync(string id)
+        {
+            await _footerTweetConfigCollection.DeleteOneAsync(x => x.FooterTweetConfigID == id);
+        }
+
+        public async Task<List<ResultFooterTweetConfigDto>> GetFooterTweetConfigAsync()
+        {
+            var values = await _footerTweetConfigCollection.Find(x => true).ToListAsync();
+
+            return _mapper.Map<List<ResultFooterTweetConfigDto>>(values);
+        }
+
+        public async Task<GetByIdFooterTweetConfigDto> GetFooterTweetConfigByIdAsync(string id)
+        {
+            var value = await _footerTweetConfigCollection.Find(x => x.FooterTweetConfigID == id).FirstOrDefaultAsync();
+
+            return _mapper.Map<GetByIdFooterTweetConfigDto>(value);
+        }
+
+        public async Task UpdateFooterTweetConfigAsync(UpdateFooterTweetConfigDto updateFooterTweetConfigDto)
+        {
+            var value = _mapper.Map<FooterTweetConfig>(updateFooterTweetConfigDto);
+
+            await _footerTweetConfigCollection.FindOneAndReplaceAsync(x => x.FooterTweetConfigID == updateFooterTweetConfigDto.FooterTweetConfigID, value);
+        }
+    }
+}
---
+++ Services/Catalog/KMNShop.Catalog/Services/FooterTweetConfigServices/IFooterTweetConfigService.cs
@@ -0,0 +1,17 @@
+﻿using KMNShop.Catalog.Dtos.FooterTweetConfigDtos;
+
+namespace KMNShop.Catalog.Services.FooterTweetConfigServices
+{
+    public interface IFooterTweetConfigService
+    {
+        Task<List<ResultFooterTweetConfigDto>> GetFooterTweetConfigAsync();
+
+        Task CreateFooterTweetConfigAsync(CreateFooterTweetConfigDto createFooterTweetConfigDto);
+
+        Task UpdateFooterTweetConfigAsync(UpdateFooterTweetConfigDto updateFooterTweetConfigDto);
+
+        Task DeleteFooterTweetConfigAsync(string id);
+
+        Task<GetByIdFooterTweetConfigDto> GetFooterTweetConfigByIdAsync(string id);
+    }
+}
