0304cb60a920bc8c1e3fe449a2315b8d91584b24
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-06-12 17:03:40 +0300

    Add DTOs for ProductImage entity (Create, Update, Result, GetById)

---
+++ Services/Catalog/KMNShop.Catalog/Dtos/ProductImageDtos/CreateProductImageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace KMNShop.Catalog.Dtos.ProductImageDtos
+{
+    public class CreateProductImageDto
+    {
+        public List<string> ImageUrls { get; set; }
+
+        public string ProductID { get; set; }
+    }
+}
---
+++ Services/Catalog/KMNShop.Catalog/Dtos/ProductImageDtos/GetByIdProductImageDto.cs
@@ -0,0 +1,11 @@
+﻿namespace KMNShop.Catalog.Dtos.ProductImageDtos
+{
+    public class GetByIdProductImageDto
+    {
+        public string ProductImageID { get; set; }
+
+        public List<string> ImageUrls { get; set; }
+
+        public string ProductID { get; set; }
+    }
+}
