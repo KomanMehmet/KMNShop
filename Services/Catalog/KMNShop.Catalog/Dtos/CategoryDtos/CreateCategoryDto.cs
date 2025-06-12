9c8c5d5f034b3efa05ec6308283178cf55c4b4e8
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-06-12 16:46:13 +0300

    Add DTOs for Category entity (Create, Update, Result, GetById)

---
+++ Services/Catalog/KMNShop.Catalog/Dtos/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace KMNShop.Catalog.Dtos.CategoryDtos
+{
+    public class CreateCategoryDto
+    {
+        public string Name { get; set; }
+
+        public string? ParentCategoryID { get; set; }
+
+        public bool IsSelectable { get; set; }
+    }
+}
---
+++ Services/Catalog/KMNShop.Catalog/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,13 @@
+﻿namespace KMNShop.Catalog.Dtos.CategoryDtos
+{
+    public class ResultCategoryDto
+    {
+        public string CategoryID { get; set; }
+
+        public string Name { get; set; }
+
+        public string? ParentCategoryID { get; set; }
+
+        public bool IsSelectable { get; set; }
+    }
+}
