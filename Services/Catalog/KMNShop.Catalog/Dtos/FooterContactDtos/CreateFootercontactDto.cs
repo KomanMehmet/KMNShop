55bcdbc5b493368417564eaef51332c25c98dd32
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2025-06-12 16:50:10 +0300

    Add DTOs for FooterContact entity (Create, Update, Result, GetById)

---
+++ Services/Catalog/KMNShop.Catalog/Dtos/FooterContactDtos/CreateFootercontactDto.cs
@@ -0,0 +1,13 @@
+﻿namespace KMNShop.Catalog.Dtos.FooterContactDtos
+{
+    public class CreateFootercontactDto
+    {
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Fax { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ Services/Catalog/KMNShop.Catalog/Dtos/FooterContactDtos/GetByIdFooterContactDto.cs
@@ -0,0 +1,15 @@
+﻿namespace KMNShop.Catalog.Dtos.FooterContactDtos
+{
+    public class GetByIdFooterContactDto
+    {
+        public string FooterContactID { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Fax { get; set; }
+
+        public string Email { get; set; }
+    }
+}
